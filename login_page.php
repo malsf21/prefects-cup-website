318af3110c28c5faf6779f89a31c2b59bea83f96
Matthew Wang <matthewang.wang@gmail.com>
2015-12-17 20:11:43 -0500

    login function

--- login_page.php
+++ login_page.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<meta name="description" content="Prefects Cup">
 		<meta name="author" content="">
-		<title>Register | Prefects Cup</title>
+		<title>Login| Prefects Cup</title>
 		<link rel="icon" href="css/favicon.ico" />
 		<link href="css/bootstrap.css" rel="stylesheet" />
 		<link href="css/base.css" rel="stylesheet"  />
@@ -35,9 +35,9 @@
 	</nav> 
 	<div class="section-one">
 	<div class="container">
-		<h1>Account Registration</h1>
-		<h3>Before you can have an account, you need to contact Matthew Wang at <a href="mailto:matthew.wang@ucc.on.ca">matthew.wang@ucc.on.ca</a></h3>
-		<form class="form-signin" action="register_function.php" method="post">
+		<h1>Account Login</h1>
+		<h3>Don't have an account? <b><a href="register_page.php">Register</a></b>.</h3>
+		<form class="form-signin" action="login_function.php" method="post">
 			<div class = "row">
 				<div class = "col-md-12">
 					<input type="email" id="email" class="form-control" placeholder="john.smith@ucc.on.ca" name="email" required="" autofocus="">
@@ -48,15 +48,10 @@
 					<input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
 				</div>
 			</div>
-			<div class = "row">
-				<div class = "col-md-12">
-					<input type="password" id="password2" name="password2" class="form-control" placeholder="Confirm Password" required="">
-				</div>
-			</div>
 			</br>
 			<div class = "row">
 				<div class = "col-md-12">
-					<button class="btn btn-lg btn-primary btn-block" type="submit" value="Login">Register</button>
+					<button class="btn btn-lg btn-primary btn-block" type="submit" value="Login">Login</button>
 				</div>
 			</div>
 		</form>
