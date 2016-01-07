b2e504cf8e73fd89f8696707638442a3f8445ac3
malsf21 <matthewang.wang@gmail.com>
2016-01-07 13:42:55 -0500

    larger button

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
 				<h1>Prefects Cup</h1>
 				</br>
 				</br>
-				<a class="page-scroll btn btn-default" href="#information" style="color:white;background-color:#337ab7;"><span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>
+				<a class="page-scroll btn btn-default btn-lg" href="#information" style="color:white;background-color:#337ab7;"><span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>
 				</span>
 			</div>
 		</section>
