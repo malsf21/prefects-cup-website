32b3fe5be55121bf3c7327c6daa6e706a5fb7850
malsf21 <matthewang.wang@gmail.com>
2016-01-07 11:59:54 -0500

    re-adds nav text

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 						</p>
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
-						<p class="navbar-text">
+						<p class="navbar-text" style="font-size:8px">
 						This website utilises <a href="http://getbootstrap.com">the bootstrap framework</a>.
 						</br>
 						Source code is available <a href="https://github.com/malsf21/prefects-cup-website">here</a>.
