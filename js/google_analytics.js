c9f511265442d6ec9e48547758660cc01b09900d
malsf21 <matthewang.wang@gmail.com>
2016-01-08 00:29:22 -0500

    implements user id

--- js/google_analytics.js
+++ js/google_analytics.js
@@ -4,4 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-72174704-1', 'auto');
+ga('set', 'userId', {{USER_ID}}); // Set the user ID using signed-in user_id.
 ga('send', 'pageview');
