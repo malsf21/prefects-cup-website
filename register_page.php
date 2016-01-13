62285c141b168da19194cf236ef6d0f08b7c0df7
malsf21 <matthewang.wang@gmail.com>
2016-01-13 09:51:06 -0500

    disables reg

--- register_page.php
+++ register_page.php
@@ -70,7 +70,7 @@
 				</br>
 				<div class = "row">
 					<div class = "col-md-12">
-						<button class="btn btn-lg btn-primary btn-block" type="submit" value="Login">Register</button>
+						<button class="btn btn-lg btn-primary btn-block" type="submit" value="Login" disabled="disabled">Register</button>
 					</div>
 				</div>
 			</form>
