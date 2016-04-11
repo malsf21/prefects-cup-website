99b76be0bd0e17978dc4c4af02157259c72d142f
Matthew Wang <matthewang.wang@gmail.com>
2016-04-10 20:10:35 -0400

    fixes up points_data format

--- admin.php
+++ admin.php
@@ -15,7 +15,7 @@
 			if(!empty($_POST)){
 				$current_time = time();
 				$new_data = $points_data;
-				$new_data["time"] = $current_time;
+				$new_data["data"]["timestamp"] = $current_time;
 				$new_data["data"]["points"] =
 				[
 					"bremner" => $_POST["bremner"],
