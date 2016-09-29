10a5f3ba3afd4f189bf4e3cc0451acac5daf724b
Matthew Wang <matthewang.wang@gmail.com>
2016-09-28 23:01:39 -0400

    removes testing data

--- js/graph-points.js
+++ js/graph-points.js
@@ -1,7 +1,5 @@
 // parsing data
-//var points_data = JSON.parse(httpGet("api/standings.php"));
-var points_data = {"date":"1473297226","bremner":"312","howard":"272","jackson":"187","martland":"253","mchugh":"138","mowbray":"187","orr":"292","scadding":"235","seaton":"260","wedd":"200","id":"1"};
-console.log(points_data);
+var points_data = JSON.parse(httpGet("api/standings.php"));
 
 //This part is just importing the variables from the JSON values
 var bremner = points_data["bremner"];
@@ -65,77 +63,3 @@ var graph = new Chart(ctx, {
     }
   }
 });
-
-/*
-// Plotly Stuff
-
-//club month info
-
-var xValue = ['Bremner\'s', 'Howard\'s', 'Jackson\'s', 'Martland\'s', 'McHugh\'s', 'Mowbray\'s', 'Orr\'s', 'Scadding\'s', 'Seaton\'s', 'Wedd\'s'];
-
-var yValue = [bremner, howard, jackson, martland, mchugh, mowbray, orr, scadding, seaton, wedd];
-
-var trace1 = {
-  x: xValue,
-  y: yValue,
-  type: 'bar',
-  marker:{
-    color: ['red', 'orange', "yellow", 'purple', 'brown', 'gray', 'blue', 'teal', 'green', 'black']
-  }
-};
-
-var trace2 = {
-  x: yValue,
-  y: xValue,
-  type: 'bar',
-  marker:{
-    color: ['red', 'orange', "yellow", 'purple', 'brown', 'gray', 'blue', 'teal', 'green', 'black']
-  },
-  orientation: 'h'
-};
-
-var data = [trace1];
-var datah = [trace2];
-
-var annotationContent = [];
-
-var layout = {
-  font: {
-    family: '"Nexa Light","Open Sans", verdana, arial, sans-serif',
-    color: 'blue'
-  },
-  paper_bgcolor: 'rgba(0,0,0,0)',
-  plot_bgcolor: 'rgba(0,0,0,0)',
-  annotations: annotationContent
-};
-
-var layouth = {
-  font: {
-    family: '"Nexa Light","Open Sans", verdana, arial, sans-serif',
-    color: 'blue'
-  },
-  paper_bgcolor: 'rgba(0,0,0,0)',
-  plot_bgcolor: 'rgba(0,0,0,0)',
-}
-
-//global info
-var tweaks = {
-  displayModeBar: false
-}
-
-for( var i = 0 ; i < xValue.length ; i++ ){
-  var result = {
-    x: xValue[i],
-    y: yValue[i],
-    text: yValue[i],
-    xanchor: 'center',
-    yanchor: 'bottom',
-    showarrow: false
-  };
-  annotationContent.push(result);
-}
-
-
-Plotly.newPlot('graph', data, layout, tweaks);
-Plotly.newPlot('graph-h', datah, layouth, tweaks);
-*/
