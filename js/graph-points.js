d44409e494641d71e4c08c4006b3fdd44f1da645
Matthew Wang <matthewang.wang@gmail.com>
2017-05-30 16:49:54 -0400

    fixes up a few small style issues with javascript

--- js/graph-points.js
+++ js/graph-points.js
@@ -1,54 +1,54 @@
 // gets data
 $.get( "api/standings.php", function( data ) {
-  var points_data = JSON.parse(data);
+  var pointsData = JSON.parse(data);
   //This part is just importing the variables from the JSON values
-  var bremner = points_data["bremner"];
-  var howard = points_data["howard"];
-  var jackson = points_data["jackson"];
-  var martland = points_data["martland"];
-  var mchugh = points_data["mchugh"];
-  var mowbray = points_data["mowbray"];
-  var orr = points_data["orr"];
-  var scadding = points_data["scadding"];
-  var seaton = points_data["seaton"];
-  var wedd = points_data["wedd"];
+  var bremner = pointsData["bremner"];
+  var howard = pointsData["howard"];
+  var jackson = pointsData["jackson"];
+  var martland = pointsData["martland"];
+  var mchugh = pointsData["mchugh"];
+  var mowbray = pointsData["mowbray"];
+  var orr = pointsData["orr"];
+  var scadding = pointsData["scadding"];
+  var seaton = pointsData["seaton"];
+  var wedd = pointsData["wedd"];
 
-  var date = new Date(points_data["date"]*1000);
-  var date = date.toDateString();
+  var date = new Date(pointsData["date"]*1000);
+  date = date.toDateString();
 
   // Charts.js!
 
   var ctx = $("#graph");
   var graph = new Chart(ctx, {
-    type: 'bar',
+    type: "bar",
     data: {
-      labels: ['Bremner\'s', 'Howard\'s', 'Jackson\'s', 'Martland\'s', 'McHugh\'s', 'Mowbray\'s', 'Orr\'s', 'Scadding\'s', 'Seaton\'s', 'Wedd\'s'],
+      labels: ["Bremner\'s", "Howard\'s", "Jackson\'s", "Martland\'s", "McHugh\'s", "Mowbray\'s", "Orr\'s", "Scadding\'s", "Seaton\'s", "Wedd\'s"],
       datasets: [{
-        label: 'Prefects Cup Points as of ' + date,
+        label: "Prefects Cup Points as of " + date,
         data: [bremner, howard, jackson, martland, mchugh, mowbray, orr, scadding, seaton, wedd],
         backgroundColor: [
-          'rgba(255, 0, 0, 0.2)',
-          'rgba(255, 127, 80, 0.2)',
-          'rgba(255, 255, 0, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(165, 42, 42, 0.2)',
-          'rgba(100, 100, 100, 0.2)',
-          'rgba(0, 200, 255, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(0, 255, 50, 0.2)',
-          'rgba(0, 0, 0, 0.2)'
+          "rgba(255, 0, 0, 0.2)",
+          "rgba(255, 127, 80, 0.2)",
+          "rgba(255, 255, 0, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(165, 42, 42, 0.2)",
+          "rgba(100, 100, 100, 0.2)",
+          "rgba(0, 200, 255, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(0, 255, 50, 0.2)",
+          "rgba(0, 0, 0, 0.2)"
         ],
         borderColor: [
-          'rgba(255, 0, 0,1)',
-          'rgba(255, 127, 80, 1)',
-          'rgba(255, 255, 0, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(165, 42, 42, 1)',
-          'rgba(100, 100, 100, 1)',
-          'rgba(0, 200, 255, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(0, 255, 50, 1)',
-          'rgba(0, 0, 0, 1)'
+          "rgba(255, 0, 0,1)",
+          "rgba(255, 127, 80, 1)",
+          "rgba(255, 255, 0, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(165, 42, 42, 1)",
+          "rgba(100, 100, 100, 1)",
+          "rgba(0, 200, 255, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(0, 255, 50, 1)",
+          "rgba(0, 0, 0, 1)"
         ],
         borderWidth: 1
       }]
@@ -73,20 +73,20 @@ $.get( "api/standings.php", function( data ) {
         easing: "easeOutQuart",
         onComplete: function () {
           var ctx = this.chart.ctx;
-          ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
-          ctx.textAlign = 'center';
-          ctx.textBaseline = 'bottom';
+          ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, "normal", Chart.defaults.global.defaultFontFamily);
+          ctx.textAlign = "center";
+          ctx.textBaseline = "bottom";
 
           this.data.datasets.forEach(function (dataset) {
             for (var i = 0; i < dataset.data.length; i++) {
               var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
-              scale_max = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._yScale.maxHeight;
-              ctx.fillStyle = '#444';
-              var y_pos = model.y - 5;
-              if ((scale_max - model.y) / scale_max >= 0.93){
-                  y_pos = model.y + 20;
+              scaleMax = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._yScale.maxHeight;
+              ctx.fillStyle = "#444";
+              var yPos = model.y - 5;
+              if ((scaleMax - model.y) / scaleMax >= 0.93){
+                  yPos = model.y + 20;
               }
-              ctx.fillText(dataset.data[i], model.x, y_pos);
+              ctx.fillText(dataset.data[i], model.x, yPos);
             }
           });
         }
